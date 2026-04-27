1baa92a08c10468d37bc59ef45ed1fd4c6d6e3b2
Antonio Morais <152793863+antoniomoraisdados@users.noreply.github.com>
2026-04-27 16:49:36 -0300

    Update analise_cancelamentos.sql

--- analise_cancelamentos.sql
+++ analise_cancelamentos.sql
@@ -1,5 +1,5 @@
 -- Consultando o total de clientes cancelados por motivo 'Preço alto'
 SELECT Motivo_Cancelamento, COUNT(ID_Cliente) AS Total
 FROM dados_vendas_petshop
-WHERE Status = 'Preço alto'
+WHERE Status = 'Cancelado'
 GROUP BY Motivo_Cancelamento;
