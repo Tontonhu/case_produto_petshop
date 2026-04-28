d7be0b8640a36e0ddae0e0e88063f9c1f3a624a8
Antonio Morais <152793863+antoniomoraisdados@users.noreply.github.com>
2026-04-28 16:39:53 -0300

    Update faturamento_perdido_churn.sql

--- sql/faturamento_perdido_churn.sql
+++ sql/faturamento_perdido_churn.sql
@@ -1,4 +1,4 @@
 -- Objetivo: Somar o valor mensal de todos os clientes com status 'Cancelado'
 SELECT SUM(valor_mensal) AS total_prejuizo_mensal
-FROM vendas_petshop
+FROM dados_vendas_petshop
 WHERE status = 'Cancelado';
